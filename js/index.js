f5b97e4c48d6ab0c748b445c280b3d8f6231bd2a
Pavithran2 <pavithrank02@gmail.com>
2024-02-07 11:52:58 +0530

    filter polyfill

--- js/index.js
+++ js/index.js
@@ -1,8 +1,22 @@
+
+//map polyfill
 Array.prototype.myMap =  function (cb) {
   let temp= [];
 
   for(let i = 0; i< this.length; i++){
     temp.push(cb(this[i], i,this))
+  }
+  return temp
+}
+
+//filter polyfill
+
+Array.prototype.myMap =  function (cb) {
+  let temp= [];
+
+  for(let i = 0; i< this.length; i++){
+    if(cb(this[i], i,this))
+    temp.push(this[i])
   }
   return temp
 }
