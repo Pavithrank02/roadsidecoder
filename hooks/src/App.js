031f7a53873287070f546f0d7205bfce1cf74c3d
Pavithran2 <pavithrank02@gmail.com>
2024-02-08 19:01:17 +0530

    useMemo

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import HookUseState from './components/UseState'
 import HookUseEffect from './components/HookUseEffect'
+import HookUseMemo from './components/HookUseMemo'
 
 const App = () => {
   return (
     <div>
       {/* <HookUseState /> */}
-      <HookUseEffect />
+      {/* <HookUseEffect /> */}
+      <HookUseMemo />
       </div>
   )
 }
