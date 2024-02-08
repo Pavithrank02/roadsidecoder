92c82660b24f89032629b350b37bb8682a2d47c6
Pavithran2 <pavithrank02@gmail.com>
2024-02-08 18:25:21 +0530

    useState

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,9 +1,21 @@
 
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [count, setCount] = useState(0)
+const handleAdd =() => {
+  setCount(count + 1)
+}
+const handleSub =() => {
+  setCount(count - 1)
+}
   return (
     <div className="App">
+      <button onClick={handleSub}>-</button>
+      <span>{count}</span>
+      <button onClick={handleAdd}>+</button>
     
     </div>
   );
