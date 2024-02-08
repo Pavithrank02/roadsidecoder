d35d49fc7a91b11e91afc824b9cfac4871357a1e
Pavithran2 <pavithrank02@gmail.com>
2024-02-08 19:12:15 +0530

    useCallback

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -2,13 +2,15 @@ import React from 'react'
 import HookUseState from './components/UseState'
 import HookUseEffect from './components/HookUseEffect'
 import HookUseMemo from './components/HookUseMemo'
+import HookUseCallback from './components/HookUseCallback'
 
 const App = () => {
   return (
     <div>
       {/* <HookUseState /> */}
       {/* <HookUseEffect /> */}
-      <HookUseMemo />
+      {/* <HookUseMemo /> */}
+      <HookUseCallback />
       </div>
   )
 }
