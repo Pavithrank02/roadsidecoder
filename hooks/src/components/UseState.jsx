939dd55a27c132237ce3bab27cbdb161c1dc4b21
Pavithran2 <pavithrank02@gmail.com>
2024-02-08 18:57:30 +0530

    state updated

---
+++ hooks/src/components/UseState.jsx
@@ -0,0 +1,37 @@
+
+import { useState } from 'react';
+import '../App.css';
+
+function HookUseState() {
+
+  const [count, setCount] = useState({
+    num: 1,
+    id: 'abcd'
+  })
+const handleAdd =() => {
+  setCount((pre) =>  {
+    return {
+      ...pre,
+      num: pre.num + 1
+    }
+  })
+}
+const handleSub =() => {
+  setCount((pre) =>  {
+    return {
+      ...pre,
+      num: pre.num - 1
+    }
+  })
+}
+  return (
+    <div className="App">
+      <button onClick={handleSub}>-</button>
+      <span>{count.num}</span>
+      <button onClick={handleAdd}>+</button>
+    
+    </div>
+  );
+}
+
+export default HookUseState;
