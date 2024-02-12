2967027fd8c9a1fcf07d84a07c0875fc03ef8faa
Pavithran2 <pavithrank02@gmail.com>
2024-02-12 14:51:02 +0530

    registerUser

--- talk-chat/backend/controller/userController.js
+++ talk-chat/backend/controller/userController.js
@@ -1,4 +1,39 @@
-const registerUser = async() = {
-  const { name, email, password, pic } = req.body
+const asyncHandler = require("express-async-handler");
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password, pic } = req.body;
 
-}
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please Enter all the Feilds");
+  }
+
+  const userExists = await User.findOne({ email })
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("user already exist")
+  }
+
+  const user = await user.create({
+    name,
+    email,
+    password,
+    pic,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      pic: user.pic,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("User not found");
+  }
+});
+
+module.exports = { registerUser }
--- talk-chat/backend/routes/userRoutes.js
+++ talk-chat/backend/routes/userRoutes.js
@@ -1,8 +1,8 @@
 const express = require('express')
-
+const { registerUser } = require('../controller/userController')
 const router = express.Router()
 
 router.route('/').post(registerUser)
-router.route('/login', authUser)
+// router.route('/login', authUser)
 
 module.exports = router
