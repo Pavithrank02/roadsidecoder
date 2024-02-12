0adb487d32ba7269bf63f52e9a7992fad661d91a
Pavithran2 <pavithrank02@gmail.com>
2024-02-12 13:05:52 +0530

    mongodb connection

--- talk-chat/backend/server.js
+++ talk-chat/backend/server.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv")
 const { chats } = require("./data/data");
+const connectDb = require("./config/db");
+const colors = require('colors')
+
 const PORT = process.env.PORT || 5000
 
 const app = express();
+dotenv.config()
+connectDb()
 app.get("/", (req, res) => {
   res.send("API is running")
 });
-dotenv.config()
 app.get("/api/chat", (req, res) => {
   res.send(chats)
 });
@@ -18,4 +22,4 @@ app.get('/api/chat/:id', (req, res) => {
   res.send(singleChat)
 
 })
-app.listen(PORT, console.log(`server is running in ${PORT}`))
+app.listen(PORT, console.log(`server is running in ${PORT}`.yellow.bold))
